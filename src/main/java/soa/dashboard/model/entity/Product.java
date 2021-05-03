5e495a1d43e167d03207f1578b5864e1b6f72ae1
elineschepers <39278079+elineschepers@users.noreply.github.com>
2021-05-03 14:04:54 +0200

    added product

---
+++ src/main/java/soa/dashboard/model/entity/Product.java
@@ -0,0 +1,75 @@
+package soa.dashboard.model.entity;
+
+import java.util.Objects;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+
+    public Product() {}
+
+    public Product(String name, int id, String description, double price){
+        this.setId(id);
+        this.setName(name);
+        this.setDescription(description);
+        this.setPrice(price);
+
+    }
+
+    public int getId() {
+        return id;
+    }
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.getId() && price == product.price && Objects.equals(name, product.name) && Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, description);
+    }
+}
